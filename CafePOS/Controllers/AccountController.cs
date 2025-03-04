b3aa9c1bec9aa6042ebc506782a3681b6b1ff4b5
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-03-04 18:42:55 +0630

    Add - done Change Password and verify Email, Logout

--- CafePOS/Controllers/AccountController.cs
+++ CafePOS/Controllers/AccountController.cs
@@ -69,5 +69,82 @@ namespace CafePOS.Controllers
             }
             return View(model);
         }
+
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> VerifyEmail(VerifyEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByEmailAsync(model.Email);
+                if(user == null)
+                {
+                    ModelState.AddModelError("", "This email doesn't register in this site");
+                    return View(model);
+                }
+                else
+                {
+                    return RedirectToAction("ChangePassword", "Account", new {userName = model.Email});
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult ChangePassword(string userName)
+        {
+            if (string.IsNullOrEmpty(userName))
+            {
+                return RedirectToAction("VerifyEmail", "Account");
+            }
+            return View(new ChangePasswordViewModel { Email = userName });
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByEmailAsync(model.Email);
+                if(user != null)
+                {
+                    var result = await userManager.RemovePasswordAsync(user);
+                    if (result.Succeeded)
+                    {
+                        result = await userManager.AddPasswordAsync(user, model.NewPassword);
+                        return RedirectToAction("LogIn", "Account");
+                    }
+                    else
+                    {
+                        foreach(var err in result.Errors)
+                        {
+                            ModelState.AddModelError("", err.Description);
+                            return View(model);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email was not found.");
+                }
+                return View(model);
+            }
+            else
+            {
+                ModelState.AddModelError("", "Something was wrong!");
+                return View(model);
+            }
+        }
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
