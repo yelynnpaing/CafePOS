926d1e239e9b53f862c9adf42dc7135af4d56da8
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-03-13 22:58:30 +0630

    Add - Item Create, Read

---
+++ CafePOS/Controllers/AdminPanel/ItemController.cs
@@ -0,0 +1,78 @@
+﻿using CafePOS.Data;
+using CafePOS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafePOS.Controllers.AdminPanel
+{
+    [Route("admin/item")]
+    public class ItemController : Controller
+    {
+        private Repository<Item> items;
+        private Repository<Category> categories;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ItemController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            items = new Repository<Item>(context);
+            categories = new Repository<Category>(context);
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [Route("list")]
+        [HttpGet]
+        public async Task<IActionResult> Items()
+        {
+            return View(await items.GetAllAsync());
+        }
+
+        [Route("addEdit")]
+        [HttpGet]
+        public async Task<IActionResult> AddEdit(Guid id)
+        {
+            ViewBag.Categories = await categories.GetAllAsync();
+            if(id == Guid.Empty)
+            {
+                ViewBag.Operation = "Item Add";
+                return View(new Item());
+            }
+            else
+            {
+                Item item = await items.GetByIdAsync(id, new QueryOptions<Item>
+                {
+                    Includes = "Category"
+                });
+                ViewBag.Operation = "Item Edit";
+                return View(item);
+            }           
+        }
+
+        [Route("addEdit")]
+        [HttpPost]
+        public async Task<IActionResult> AddEdit(Item item, Guid CatId)
+        {
+            ViewBag.Categories = await categories.GetAllAsync();
+            if (ModelState.IsValid)
+            {
+                if(item.ImageFile != null)
+                {
+                    string imgaeUploader = Path.Combine(_webHostEnvironment.WebRootPath, "Images");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + item.ImageFile.FileName;
+                    string filePath = Path.Combine(imgaeUploader, uniqueFileName);
+                    using(var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await item.ImageFile.CopyToAsync(fileStream);
+                    }
+                    item.ImageUrl = uniqueFileName;
+                    if(item.ItemId == Guid.Empty)
+                    {
+                        item.CategoryId = CatId;
+                        item.IsAvailable = true;
+                        await items.AddAsync(item);
+                        return RedirectToAction("Index", "Item");
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Item");
+        }
+    }
+}
--- CafePOS/Models/Item.cs
+++ CafePOS/Models/Item.cs
@@ -1,24 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CafePOS.Models
 {
     public class Item
     {
+        [Key]
         public Guid ItemId { get; set; }
+        [Required]
         public string ItemName { get; set; }
         public string Description { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public Guid CategoryId { get; set; }
         [NotMapped]
         public IFormFile? ImageFile { get; set; }
         public string ImageUrl { get; set; } = "https://via.placeholder.com/150";
         [ValidateNever]
+        [ForeignKey("CategoryId")]
         public Category? Category { get; set; }
+        [Required]
         public int StockQuantity { get; set; }
-        public bool IsAvailable { get; set; }
+        public bool IsAvailable { get; set; } 
         public  DateTime CreatedAt {get; set;}
         public DateTime UpdatedAt { get; set; }
+        [ValidateNever]
+        public ICollection<OrderItem>? OrderItems { get; set; }
     }
 }
