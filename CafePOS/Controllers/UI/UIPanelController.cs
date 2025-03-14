8ce3785907f9453a9127def01f55dcdc7ffecaa8
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-03-15 03:22:42 +0630

    Add - UI Layouting

---
+++ CafePOS/Controllers/UI/UIPanelController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CafePOS.Controllers.UI
+{
+    [Route("mm-cafe")]
+    public class UIPanelController : Controller
+    {
+        [Route("home")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
