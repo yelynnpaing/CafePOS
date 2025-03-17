2fe5aa92e31a062fd9d3ca6bd269008da0761593
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-03-17 23:08:09 +0630

    Add - Read and Normal Search Menu Item List

---
+++ CafePOS/Controllers/UI/UIItemController.cs
@@ -0,0 +1,32 @@
+﻿using CafePOS.Data;
+using CafePOS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafePOS.Controllers.UI
+{
+    [Route("mm-cafe/items/")]
+    public class UIItemController : Controller
+    {
+        private Repository<Item> items;
+        private Repository<Category> categories;
+
+        public UIItemController(AppDbContext context)
+        {
+            items = new Repository<Item>(context);
+            categories = new Repository<Category>(context);
+        }
+
+
+        [Route("list")]
+        [HttpGet]
+        public async Task<IActionResult> List(string searchString)
+        {
+            var Items = await items.GetAllAsync();
+            if(!String.IsNullOrEmpty(searchString))
+            {
+                Items = Items.Where(i => i.ItemName.Contains(searchString)).ToList();
+            }
+            return View(Items);
+        }
+    }
+}
