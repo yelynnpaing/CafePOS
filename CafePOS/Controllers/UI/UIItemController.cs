4fdfbf08cf131ee8c4cc64806cfb6ac54b6dc146
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-03-18 16:38:02 +0630

    Add - Item filter with Category , Show Item grouped with category

--- CafePOS/Controllers/UI/UIItemController.cs
+++ CafePOS/Controllers/UI/UIItemController.cs
@@ -7,26 +7,49 @@ namespace CafePOS.Controllers.UI
     [Route("mm-cafe/items/")]
     public class UIItemController : Controller
     {
-        private Repository<Item> items;
-        private Repository<Category> categories;
+        private Repository<Item> _items;
+        private Repository<Category> _categories;
 
         public UIItemController(AppDbContext context)
         {
-            items = new Repository<Item>(context);
-            categories = new Repository<Category>(context);
+            _items = new Repository<Item>(context);
+            _categories = new Repository<Category>(context);
         }
 
 
         [Route("list")]
         [HttpGet]
-        public async Task<IActionResult> List(string searchString)
+        public async Task<IActionResult> List(string searchString, Guid? CategoryId)
         {
-            var Items = await items.GetAllAsync();
-            if(!String.IsNullOrEmpty(searchString))
+            var Items = await _items.GetAllAsync();
+            var Categories = await _categories.GetAllAsync();
+
+            //use category filter if a category is selected
+            if (CategoryId.HasValue)
+            {
+                Items = Items.Where(i => i.CategoryId == CategoryId.Value).ToList();
+            }
+
+            // use search filter if search string is provided
+            if (!String.IsNullOrEmpty(searchString))
             {
                 Items = Items.Where(i => i.ItemName.Contains(searchString)).ToList();
             }
-            return View(Items);
+
+            // use Group filtered items by category
+            var groupedItems = Categories.Select(category => new
+            {
+                Category = category,
+                Items = Items.Where(i => i.CategoryId == category.CategoryId).ToList()
+            }).ToList();
+
+            ViewBag.GroupItems = groupedItems;
+            ViewBag.SearchString = searchString;
+            ViewBag.Categories = Categories;
+            ViewBag.SelectedCategoryId = CategoryId;
+
+            return View();
         }
+                
     }
 }
