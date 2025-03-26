6171a67f655695b27ab8bb232306cf8f8c48085a
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2025-03-27 01:35:01 +0630

    Add - Home UI for Tables

--- CafePOS/Controllers/UI/UIPanelController.cs
+++ CafePOS/Controllers/UI/UIPanelController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CafePOS.Data;
+using CafePOS.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CafePOS.Controllers.UI
@@ -6,12 +8,24 @@ namespace CafePOS.Controllers.UI
     [Authorize]
     [Route("mm-cafe/")]
     public class UIPanelController : Controller
-    {        
+    {
+        private readonly AppDbContext _context;
+        private Repository<Item> _items;
+        private Repository<CafeTable> _cafeTables;
+
+        public UIPanelController(AppDbContext context)
+        {
+            _context = context;
+            _items = new Repository<Item>(context);
+            _cafeTables = new Repository<CafeTable>(context);
+        }
+
         [Route("home")]
         [HttpGet]
-        public IActionResult Home()
+        public async Task<IActionResult> Home()
         {
-            return View();
+            var CafeTables = await _cafeTables.GetAllAsync();            
+            return View(CafeTables);
         }
     }
 }
